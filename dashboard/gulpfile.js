3bbb23c902f5759f446939a2c5e5c804bda8ae9f
Jordan Wright <jwright@duosecurity.com>
2019-01-19 21:55:11 -0600

    Moving away from Bower in favor of just Yarn + Gulp

--- dashboard/gulpfile.js
+++ dashboard/gulpfile.js
@@ -19,35 +19,36 @@ gulp.task('scripts', function () {
         .pipe(plugins.uglify().on('error', function (e) {
             console.log(e);
         }))
-        .pipe(plugins.rename({ suffix: '.min' }))
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('static/dist/js'));
 });
 
 // Concatenate vendor JS
 gulp.task('vendor-js', function () {
-    var filterJS = plugins.filter('**/*.js', { restore: true });
+    var filterJS = plugins.filter('**/*.js', {
+        restore: true
+    });
 
-    return gulp.src('./bower.json')
-        .pipe(plugins.mainBowerFiles({
-            overrides: {
-                // gentelella
-                gentelella: {
-                    main: [
-                        './vendors/jquery/dist/jquery.min.js',
-                        './vendors/bootstrap/dist/js/bootstrap.min.js',
-                        './vendors/fastclick/lib/fastclick.js',
-                        './vendors/nprogress/nprogress.js',
-                        './vendors/pnotify/dist/pnotify.js',
-                        './vendors/pnotify/dist/pnotify.buttons.js',
-                        './vendors/moment/moment.js',
-                        './production/js/datepicker/daterangepicker.js',
-                        './vendors/datatables.net/js/jquery.dataTables.min.js',
-                        './vendors/datatables.net-bs/js/dataTables.bootstrap.min.js',
-                        './vendors/select2/dist/js/select2.full.min.js'
-                    ]
-                }
-            }
-        }))
+    // TODO: Eventually we'll use proper imports + webpack for this.
+    return gulp.src([
+            './node_modules/dompurify/dist/purify.min.js',
+            './node_modules/gentelella/vendors/jquery/dist/jquery.min.js',
+            './node_modules/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js',
+            './node_modules/gentelella/vendors/fastclick/lib/fastclick.js',
+            './node_modules/gentelella/vendors/nprogress/nprogress.js',
+            './node_modules/gentelella/vendors/pnotify/dist/pnotify.js',
+            './node_modules/gentelella/vendors/pnotify/dist/pnotify.buttons.js',
+            './node_modules/gentelella/vendors/moment/moment.js',
+            './node_modules/gentelella/production/js/datepicker/daterangepicker.js',
+            './node_modules/gentelella/vendors/datatables.net/js/jquery.dataTables.min.js',
+            './node_modules/gentelella/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js',
+            './node_modules/gentelella/vendors/select2/dist/js/select2.full.min.js',
+            './node_modules/highcharts/highcharts.js',
+            './node_modules/sweetalert2/sweetalert2.min.js',
+            './node_modules/underscore/underscore-min.js'
+        ])
         .pipe(filterJS)
         .pipe(plugins.concat('vendor.min.js'))
         .pipe(plugins.uglify())
@@ -56,25 +57,22 @@ gulp.task('vendor-js', function () {
 
 // Move over vendor CSS
 gulp.task('vendor-css', function () {
-    var filterCSS = plugins.filter('**/*.css', { restore: true });
+    var filterCSS = plugins.filter('**/*.css', {
+        restore: true
+    });
 
-    return gulp.src('./bower.json')
-        .pipe(plugins.mainBowerFiles({
-            overrides: {
-                // gentelella
-                gentelella: {
-                    main: [
-                        './vendors/bootstrap/dist/css/bootstrap.min.css',
-                        './vendors/font-awesome/css/font-awesome.css',
-                        './vendors/pnotify/dist/pnotify.css',
-                        './vendors/pnotify/dist/*.css',
-                        './vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css',
-                        './vendors/datatables.net-bs/css/dataTables.bootstrap.min.css',
-                        './vendors/select2/dist/css/select2.min.css',
-                    ]
-                }
-            }
-        }))
+    return gulp.src([
+            './node_modules/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css',
+            './node_modules/gentelella/vendors/font-awesome/css/font-awesome.css',
+            './node_modules/gentelella/vendors/pnotify/dist/pnotify.css',
+            './node_modules/gentelella/vendors/pnotify/dist/*.css',
+            './node_modules/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css',
+            './node_modules/gentelella/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css',
+            './node_modules/gentelella/vendors/select2/dist/css/select2.min.css',
+            './node_modules/highcharts/css/highcharts.css',
+            './node_modules/sweetalert2/dist/sweetalert2.min.css',
+            './node_modules/pretty-checkbox/dist/pretty-checkbox.min.css'
+        ])
         .pipe(filterCSS)
         .pipe(plugins.concat('vendor.min.css'))
         .pipe(gulp.dest('static/dist/css'));
@@ -88,27 +86,25 @@ gulp.task('fonts', function () {
 
 // Move over vendor fonts
 gulp.task('vendor-fonts', function () {
-    return gulp.src('./bower.json')
-        .pipe(plugins.mainBowerFiles({
-            overrides: {
-                // gentelella
-                gentelella: {
-                    main: [
-                        './vendors/font-awesome/fonts/fontawesome-webfont.woff',
-                        './vendors/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2',
-                        './vendors/font-awesome/fonts/fontawesome-webfont.woff2'
-                    ]
-                }
-            }
+    return gulp.src([
+            './node_modules/gentelella/vendors/font-awesome/fonts/fontawesome-webfont.woff',
+            './node_modules/gentelella/vendors/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2',
+            './node_modules/gentelella/vendors/font-awesome/fonts/fontawesome-webfont.woff2'
+        ])
+        .pipe(plugins.rename({
+            dirname: ''
         }))
-        .pipe(plugins.rename({ dirname: '' }))
         .pipe(gulp.dest('static/dist/fonts'));
 });
 
 // Process and minimize SASS -> CSS
 gulp.task('sass', function () {
-    return plugins.rubySass('static/src/scss/custom.scss', { style: 'compressed' })
-        .pipe(plugins.rename({ suffix: '.min' }))
+    return plugins.sass('static/src/scss/custom.scss', {
+            style: 'compressed'
+        })
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('static/dist/css'));
 });
 
@@ -131,10 +127,7 @@ gulp.task('watch', function () {
 
     // Font changes
     gulp.watch('static/src/fonts/*', ['fonts']);
-
-    // Vendor JS
-    gulp.watch(['bower.json', 'bower_modules/**/*'], ['vendor-js', 'vendor-css', 'vendor-fonts']);
 });
 
 // Default
-gulp.task('default', ['lint', 'vendor-js', 'vendor-css', 'vendor-fonts', 'fonts', 'scripts', 'sass', 'images', 'watch']);
+gulp.task('default', ['lint', 'vendor-js', 'vendor-css', 'vendor-fonts', 'fonts', 'scripts', 'sass', 'images']);
